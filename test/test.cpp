ad24ce6cd14ee0f625be44cf31210da0538e7cde
libr <libr@info2soft.com>
2025-07-03 10:10:59 +0800

    [CU]: code update

--- test/test.cpp
+++ test/test.cpp
@@ -1,36 +1,34 @@
 // compile app with spdk feature
 #include "test.hpp"
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <signal.h>
+using namespace std;
+pid_t pid = getpid();
 
-
+static void timmer(int n) {
+    if(n <= 0) {
+        return;
+    }
+    int i = 0;
+    while(i < n) {
+        sleep(1);
+        ++i;
+    }
+    kill(pid, SIGKILL);
+}
 
 int main(int argc, char* argv[]) {
     
-    // struct spdk_env_opts opts;
-    struct spdk_app_opts app_opts = {};
-    spdk_app_opts_init(&app_opts, sizeof(struct spdk_app_opts));
+    cout << "pid : " << pid << endl;
+    
+    CSpdkControl spdkControl("config1.json");
+    spdkControl.start_spdk();
 
-    // use if this program run in lib
-    // spdk_env_opts_init(&opts); 
-    // spdk_env_init(&opts);
-
-    // init app_opts
-    app_opts.name = "Mr.Li";
-    app_opts.rpc_log_file = fopen("./test.log", "w");
-    app_opts.rpc_log_level = SPDK_LOG_DEBUG;
-    app_opts.interrupt_mode = true;
-    app_opts.json_config_file = argv[1];
-
-    myctx *s = (myctx*)malloc(sizeof(myctx));
-
-
-    // start spdk reactor and this program will be hold until spdk_app_stop() called
-    int rc = spdk_app_start(&app_opts, test_entry, s);
-    if(rc != 0) {
-        log_info("spdk_app exit with error");
-    }
-
-
-
-    return 0;
+    cout << "start spdk" << endl;
+    sleep(5);
+    spdkControl.stop_spdk();
+    cout << "stop spdk" << endl;
 }
 
