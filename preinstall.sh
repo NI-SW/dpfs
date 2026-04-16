51505c2eeaef142fd69d6106915246c55e0d1527
libr <libr@info2soft.com>
2026-04-16 14:06:43 +0800

    [CU]: update item delete func for collect

--- preinstall.sh
+++ preinstall.sh
@@ -2,6 +2,6 @@
 
 yum install -y gcc-c++ libaio-devel numactl-devel openssl-devel CUnit-devel libuuid-devel python3-pip libibverbs 
 yum install -y libnvme-devel librdmacm rdma-core libnuma-devel ncurses-devel nvme-cli nvmetcli
-yum install -y rapidjson-devel grpc-devel grpc-plugins grpc-cpp grpc-data grpc
+yum install -y rapidjson-devel grpc-devel grpc-plugins grpc-cpp grpc-data grpc librdmacm-devel libibverbs-utils fuse3-devel meson
 pip install pyelftools
 
