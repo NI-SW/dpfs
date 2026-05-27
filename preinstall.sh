13d51dc0495414adb0cefc4dff8bc0f582d27d25
libr <libr@info2soft.com>
2026-05-27 07:12:54 -0400

    [CU]: update prerequest file

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 yum install -y gcc-c++ libaio-devel numactl-devel openssl-devel CUnit-devel libuuid-devel python3-pip libibverbs 
-yum install -y libnvme-devel librdmacm rdma-core libnuma-devel ncurses-devel nvme-cli nvmetcli
+yum install -y libnvme-devel golang librdmacm rdma-core libnuma-devel ncurses-devel nvme-cli nvmetcli
 yum install -y rapidjson-devel grpc-devel grpc-plugins grpc-cpp grpc-data grpc librdmacm-devel libibverbs-utils fuse3-devel meson
+yum install -y librdmacm rdma-core-devel fuse3-devel meson
 pip install pyelftools
 
